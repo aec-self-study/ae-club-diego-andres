5d8d4d346975c520fcc032d94d0af85b454eab58
IOxymoron <osoetan@afexnigeria.com>
2024-01-22 16:37:54 +0100

    This is to test the merge conflict, using the forked repo option

--- conflict_practice.sql
+++ conflict_practice.sql
@@ -1,1 +1,11 @@
 -- this comment has been changed to simulate the merge conflict
+
+-- so this is for the test using the forked repo version, will add a fake select statement below
+
+select * 
+
+from an_imaginary_table
+
+where a_col in ('animals', 'humans', 'life')
+
+limit 5
